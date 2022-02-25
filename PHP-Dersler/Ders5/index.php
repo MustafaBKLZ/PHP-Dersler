a63f42862867e3e5f3667ce6fd654da14ad64026
Mustafa BÜKÜLMEZ <mustafabukulmez3446@gmail.com>
2022-02-25 16:37:31 +0300

    Update index.php

--- PHP-Dersler/Ders5/index.php
+++ PHP-Dersler/Ders5/index.php
@@ -1,1 +1,16 @@
-<?php require_once($_SERVER['DOCUMENT_ROOT'] . "/classes/ImportClasses.php");  ?>
+<?php 
+require_once($_SERVER['DOCUMENT_ROOT'] . "/classes/ImportClasses.php"); 
+
+$sonuclar = $DB->ShowTable("select * from table where column1 = ? and column2 = ?", array(1,2));
+
+require_once($_SERVER['DOCUMENT_ROOT'] . "/includes/HTMLHead.php");  
+// includes klasöründe, sayfalarımızın HTML Head etiketleri arasında olması gereken kodları tuttuğumuz ayrı bir dosya varsayalım.
+?>
+
+<body>
+
+<h1> index.php </h1>
+
+
+</body>
+</html>
