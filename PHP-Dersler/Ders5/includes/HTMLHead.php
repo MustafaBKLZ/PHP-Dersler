4d35a508603a027501c137fc27a45b573c7f0d66
Mustafa BÜKÜLMEZ <mustafabukulmez3446@gmail.com>
2022-02-25 16:34:13 +0300

    Create HTMLHead.php

---
+++ PHP-Dersler/Ders5/includes/HTMLHead.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang=tr">
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta content='text/html; charset=UTF-8' http-equiv='Content-Type' />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">    
+                                                                                         
+    <title>Mustafa BÜKÜLMEZ</title>
+                                                                                         
+    <meta name="description" content="Mustafa BÜKÜLMEZ">
+    <meta name="keywords" content="Mustafa BÜKÜLMEZ">
+    <meta name="author" content="Mustafa BÜKÜLMEZ">
+    <link rel="icon" type="image/x-icon" href="/img/1.png">
+    <link rel="stylesheet" href="/dist/style.css">
+
+</head>
